ae2b872f753769f915155fc4e54d3cae8a6e0f24
Abdulsaid004 <abdulsaidzyldyzbekov5@gmail.com>
2026-04-27 18:33:45 +0300

    fix main polling and db init

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
-import asyncio 
-from aiogram import Bot, Dispatcher, types # type: ignore
-from aiogram.filters import CommandStart # type: ignore
-from keyboards.main_kb import main_keyboard # type: ignore
-from handlers.order import router as order_router  # type: ignore
-from handlers.db import init_db  # type: ignore
-
+import asyncio
 import os
 
-BOT_TOKEN = "8099643683:AAHIgF929_5x_DOLXZj3gTXDNd1Zlkc7MNg"
- 
+from aiogram import Bot, Dispatcher, types # type: ignore
+from aiogram.filters import CommandStart # type: ignore
+
+from keyboards.main_kb import main_keyboard # type: ignore
+from handlers.order import router as order_router # type: ignore
+from handlers.db import init_db # type: ignore
+
+
+BOT_TOKEN = "ТВОЙ_ТОКЕН_СЮДА"
+
 
 async def main() -> None:
+    # 1. Инициализация БД
     await init_db()
 
+    # 2. Бот и диспетчер
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
 
+    # 3. Подключаем роутеры
     dp.include_router(order_router)
-    
-    await dp.start_polling(bot)
 
+    # 4. Хендлеры
     @dp.message(CommandStart())
     async def start(message: types.Message) -> None:
         await message.answer(
@@ -34,12 +38,11 @@ async def main() -> None:
         print("THREAD ID:", message.message_thread_id)
         print("TEXT:", message.text)
 
-
+    # ❗ ВАЖНО: только ОДИН запуск в самом конце
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    import asyncio
     asyncio.run(main())
 
 
